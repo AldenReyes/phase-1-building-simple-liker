a741f6e09a1d57edfe8fa58cbd28f2fb5a401ae7
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-04-25 01:12:13 -0700

    Completed Lab; connected server to event function + displays error on server error

--- main.js
+++ main.js
@@ -3,26 +3,33 @@ const EMPTY_HEART = "♡";
 const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
+const errorModal = document.getElementById("modal");
+errorModal.setAttribute("class", "hidden");
+
 document.querySelectorAll(".like-glyph").forEach((heart) => {
-  heart.addEventListener("click", changeHeart);
+  heart.addEventListener("click", (e) => {
+    mimicServerCall()
+      .then(() => changeHeart(e))
+      .catch(handleError);
+  });
 });
 
-
-// Initial Functions
-document.getElementById("modal").setAttribute("class", "hidden")
-
 // Handler Functions
-
 function changeHeart(event) {
-  const likeGlyph = event.target
-  console.log(likeGlyph)
+  const likeGlyph = event.target;
+  console.log(likeGlyph);
   if (likeGlyph.textContent === EMPTY_HEART) {
     likeGlyph.textContent = FULL_HEART;
-    likeGlyph.classList.add("activated-heart")
-  } else if (likeGlyph.textContent = FULL_HEART) {
+    likeGlyph.classList.add("activated-heart");
+  } else if ((likeGlyph.textContent = FULL_HEART)) {
     likeGlyph.textContent = EMPTY_HEART;
-    likeGlyph.classList.remove("activated-heart")
+    likeGlyph.classList.remove("activated-heart");
   }
+}
+function handleError(message) {
+  errorModal.classList.remove("hidden");
+  document.getElementById("modal-message").textContent = message;
+  setTimeout(() => errorModal.classList.add("hidden"), 3000);
 }
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
